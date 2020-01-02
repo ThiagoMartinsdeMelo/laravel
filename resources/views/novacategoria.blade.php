903867801548246c04a5d55d43dd88a53144c7c4
thiago <thiagompdi@gmail.com>
2020-01-02 19:23:38 -0300

    cadastrado categorias

---
+++ resources/views/novacategoria.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app', ['current' => 'categorias'])
+@section('body')
+    <div class="card border">
+        <div class="card-body">
+            <form action="/categorias" method="POST">
+                <div class="form-group"
+            </form>
+        </div>
+    </div>
+@endsection
